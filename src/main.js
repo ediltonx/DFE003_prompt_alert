01a6834607d1429427c61fe77dc27b85d259773a
ediltonx <ediltonx@hotmail.com>
2023-10-03 10:53:36 -0300

    exercicio resolvido

---
+++ src/main.js
@@ -0,0 +1,25 @@
+// questão 1
+
+var num = prompt("Digite um valor: ")
+
+window.alert("O dobro do valor é "+num*2)
+
+
+//questão 2
+
+let conta = prompt("Digite o valor da conta: ")
+
+let gorjeta = prompt ("Digite a gorjeta pro garçom: ")
+
+let total = conta + gorjeta
+
+window.alert("O valor total a pagar é " + (total))
+
+// questão 3
+
+let bill = prompt("Digite o valor da conta: ")
+
+let people = prompt ("Digite quantas pessoas vão pagar: ")
+
+window.alert("O valor a ser pago por cada um é  " + bill/people)
+
